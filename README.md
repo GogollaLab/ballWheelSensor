31a5055e7f791017c77c67d855764fade5e0891d
NDolensek <n.dolensek@gmail.com>
2020-02-04 01:32:35 +0100

    updated readme, added analysis script

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # opticalBallWheelSensor
 Tracking virtual reality wheels and running balls using optical sensors
+
+The scripts above are written to run on a Raspberry Pi (tested versions: 2B, 3B, 3B+, 4B),
+and record all outputs of a desired computer mouse sensor which enables precise logging 
+of movement speed and direction
+
+This is achieved by hooking into a USB interface associated with a specified USB HID 
+(human interface device, in this case, a computer mouse) and capturing all its output
+instead of letting the kernel.
+
+The scripts can be used with any mouse, provided that the idVendor and idProduct
+arguments are set correctly. Consult [this](http://the-sz.com/products/usbid/) website if you are not sure where to find these values
+for your sensor.
+
+
+There are 3 variations of the script:  
+* Time1.py will record for a specified number of seconds  
+* TTL_Time1.py will record for a specified number of seconds from the moment of detecting a HIGH value on GPIO pin 21  
+* TTL1.py will record from the moment of detecting a HIGH value on GPIO pin 21 and until the value changes to LOW  
+
+Execute all scripts using terminal by navigating to a containing directory and executing the following (enter the duration of recording 
+as a number of second and pressing enter if prompted):
+```
+sudo python Time1.py
+```
+
+All scripts are set to print timestamped output, which will be output directly to the terminal in the above
+use-case. This enables easy over-network recording in a headless setup using SSH if desired. 
+For local recording, the code can easily be adapted to write directly to a file or the output can be captured and
+recorded in terminal like this:
+```
+sudo python Time1.py >> recording1.txt
+```
+
+## Output
+
+Note that output:
+```
+0.0199191570282 , array('B', [0, 0, 226, 255, 17, 0, 0, 0])
+0.0678420066833 , array('B', [0, 0, 255, 255, 0, 0, 0, 0])
+0.147843122482 , array('B', [0, 0, 0, 0, 1, 0, 0, 0])
+0.16383600235 , array('B', [0, 0, 255, 255, 0, 0, 0, 0])
+0.179836988449 , array('B', [0, 0, 0, 0, 1, 0, 0, 0])
+```
+is completely unprocessed by default for performance reasons and not
+very human-readable. 
+
+
+## Output processing
+
+### Output cleaning
+
+### Output decoding
+
+Using `ballSensor_analysis_cleaned.R` cleaned output can be converted into running speed values.
+The script decodes and bins the data to a desired frequency to ease synchronisation with other recordings 
+(output frequency is set by editing the value of binsPerSecond variable). It then calculates 
+an "absolute" vector of movement per a datapoint by finding a hypotenuse between the 2 axes of movement 
+the mouse sensor records from. Effectively that is distance moved in the period of time defined by 
+acquisition frequency, which equals speed. 
---
+++ ballSensor_analysis_cleaned.R
@@ -0,0 +1,153 @@
+library(plyr) #conditional replacement of values, revaluing
+
+
+
+
+
+
+acqFreq <- 62.5 #acquisition frequency of the ball sensor; DEFAULT IS 62.5
+binsPerSecond <- 10 #define the number of bins per second
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ballSensorProcess <- function(filename) {
+  data <- read.table(filename)
+  
+  
+  
+  
+  
+  
+  
+  #for direction and velocity only the 3,4,5,6 positions are relevant, that is columns V4, V5, V6, V7
+  data <- data[,-(2:3)] #remove V2, V3
+  data <- data[,-(6:7)] #remove V7, V8
+  
+  colnames(data) <- c("time", "LRvel", "LR", "UDvel", "UD")
+  
+  #remove all rows containing any wrong value - corrupted recording
+  test1 <- table(data$UD)
+  lenTest1 <- length(test1)
+  test1names <- dimnames(test1)
+  
+  #if more than 2 values (0, 255) in data, remove all rows with those vals
+  if(lenTest1 > 2){
+    for(i in 2:(lenTest1 - 1)){
+      temp1 <- as.numeric(test1names[[1]][[i]])
+      
+      wrong1 <- subset(data, UD == temp1)
+      wrong2 <- as.numeric(row.names(wrong1))
+      
+      data <- data[-c(wrong2),]
+      
+      rownames(data) <- seq(length=nrow(data)) #reset row names (numbers) 
+      
+    }
+  }
+  
+  test1 <- table(data$LR)
+  lenTest1 <- length(test1)
+  test1names <- dimnames(test1)
+  
+  #if more than 2 values (0, 255) in data, remove all rows with those vals
+  if(lenTest1 > 2){
+    for(i in 2:(lenTest1 - 1)){
+      temp1 <- as.numeric(test1names[[1]][[i]])
+      
+      wrong1 <- subset(data, LR == temp1)
+      wrong2 <- as.numeric(row.names(wrong1))
+      
+      data <- data[-c(wrong2),]
+      
+      rownames(data) <- seq(length=nrow(data)) #reset row names (numbers) 
+      
+    }
+  }
+  
+  rownames(data) <- seq(length=nrow(data)) #reset row names (numbers) 
+  
+  #inspiration from: http://stackoverflow.com/questions/8214303/conditional-replacement-of-values-in-a-data-frame
+  data$LRvel[data$LR == 255] <- mapvalues(data$LRvel[data$LR == 255], rev(1:255), c(1:255), warn_missing = FALSE)
+  data$UDvel[data$UD == 255] <- mapvalues(data$UDvel[data$UD == 255], rev(1:255), c(1:255), warn_missing = FALSE)
+  
+  data1 <- data
+  
+  #transfort LR, UD values into more meaningful factors
+  data1$LR <- as.factor(data1$LR)
+  data1$UD <- as.factor(data1$UD)
+  
+  
+  data1$LR <- revalue(data1$LR, c("255" = "L", "0" = "R"), warn_missing = FALSE)
+  data1$UD <- revalue(data1$UD, c("255" = "U", "0" = "D"), warn_missing = FALSE)
+  
+  
+  data1$dir <- paste0(data1$LR, data1$UD)
+  data1$dir <- as.factor(data1$dir)
+  
+  #separate data by the axes - LR and UD are on the same axes, just moving in a differnt direction
+  #instead always starting from 0, make negative vals for the opposite direction
+  # U + / D - 
+  # R + / L -
+  data1$LRvel[data1$LR == "L"] <- data1$LRvel[data1$LR == "L"] * (-1)
+  data1$UDvel[data1$UD == "D"] <- data1$UDvel[data1$UD == "D"] * (-1)
+  
+  #the vector of movement is the hypotenuse in a right-angled triangle with catheti of a and b (UDvel and LRvel for each timepoint)
+  data1$hypo <- sqrt((data1$LRvel)^2 + (data1$UDvel)^2)
+  
+  #plot(data1$time, data1$hypo, type = "l")
+  
+  
+  
+  #binning
+  binVar <- NULL
+  timeStep <- 1/binsPerSecond
+  for(i in seq(0, floor(max(data1$time)), timeStep)){
+    bin1 <- subset(data1, time > i & time < (i + timeStep))
+    mean1 <- sum(bin1$hypo)/(acqFreq/binsPerSecond)
+    
+    binVar$time[(i * binsPerSecond) + 1] <- i
+    binVar$mean[(i * binsPerSecond) + 1] <- mean1
+  }
+  
+  binVar <- data.frame(binVar)
+  
+  
+  
+  
+  
+  
+  #write to file
+  noExtFilename <- tools::file_path_sans_ext(filename) #remove extension from the filename
+  outputFilename <- paste0("processed/", noExtFilename, "__binned_", binsPerSecond, "Hz")
+  outputFilename <- paste(outputFilename, "txt", sep = ".")
+  
+  write.table(binVar, outputFilename, sep="\t", row.names = F, col.names = T)
+}
+
+
+
+
+
+
+
+
+
+files <- list.files(pattern = "*.txt")
+
+dir.create("processed")
+for(i in 1:length(files)){
+  filename <- files[i]
+  ballSensorProcess(filename)
+}
