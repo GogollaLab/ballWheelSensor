3d4c98d8845846ff69b04a7bbf6eb8cda32eb184
NDolensek <n.dolensek@gmail.com>
2020-02-06 02:51:13 +0100

    updated for python3 and cleaned up code

--- TTL1.py
+++ TTL1.py
@@ -5,11 +5,8 @@ import usb.util
 import time
 import RPi.GPIO as GPIO
 
-# decimal vendor and product values
-#dev = usb.core.find(idVendor=0x046D, idProduct=0xC24E)
-# or, uncomment the next line to search instead by the hexidecimal equivalent
-#dev = usb.core.find(idVendor=0x45e, idProduct=0x77d)
 dev = usb.core.find(idVendor=0x046d, idProduct=0xc332) #Logitech G502 Proteus Spectrum mouse
+
 # first endpoint
 interface = 0
 endpoint = dev[0][(0,0)][0]
@@ -20,14 +17,13 @@ if dev.is_kernel_driver_active(interface) is True:
   dev.detach_kernel_driver(interface)
   # claim the device
   usb.util.claim_interface(dev, interface)
-##collected = 0
-##attempts = 100+
+
 
 
 
 GPIO.setmode(GPIO.BCM) #sets processor GPIO numbering sys (GPIO No.)
 GPIO.setup(21, GPIO.IN) #sets pin GPIO 21 as input
-#GPIO.input(21) #digitalRead(21); 0 for LOW, 1 for HIGH
+
 pinVal = GPIO.input(21)
 
 while pinVal == 0:
@@ -39,7 +35,7 @@ while pinVal == 1 :
     try:
         data = dev.read(endpoint.bEndpointAddress,endpoint.wMaxPacketSize)
         timestamp = time.time() - startTime
-        print timestamp, "," , data
+        print(timestamp, "," , data)
         pinVal = GPIO.input(21)
     except usb.core.USBError as e:
         data = None
@@ -47,16 +43,7 @@ while pinVal == 1 :
             continue
 
 
-##while collected < attempts :
-##    try:
-##        data = dev.read(endpoint.bEndpointAddress,endpoint.wMaxPacketSize)
-##        collected += 1
-##        timestamp = time.time() - startTime
-##        print timestamp, "," , data
-##    except usb.core.USBError as e:
-##        data = None
-##        if e.args == ('Operation timed out',):
-##            continue
+
 # release the device
 usb.util.release_interface(dev, interface)
 # reattach the device to the OS kernel
