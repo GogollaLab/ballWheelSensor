9cca3503e8169ead533bdfb5ddf1b29f87e066ab
NDolensek <n.dolensek@gmail.com>
2020-02-06 03:01:19 +0100

    added new replace parameters

--- cleanBallData_r1.py
+++ cleanBallData_r1.py
@@ -1,7 +1,6 @@
 import os
 import glob
 
-#folderPath = "E:/ballAnalysisTest1/ff/"
 folderPath = os.getcwd()
 
 outputfolderPath = folderPath + "/cl/" #cl subfolder will be created if it doesn't exist
@@ -10,4 +9,5 @@ for i in glob.glob(folderPath + "/*.txt"):
     with open(i) as rawfile, open(outputfolderPath + os.path.basename(i)[:-4] + "_cl.txt", "w") as cleanfile:
 
         for line in rawfile:
-            cleanfile.write(line.replace("How long is the imaging session (answer in seconds; time from TTL HIGH to end of the aqusition)?", "").replace(", array('B', [", "").replace("])", "").replace(",", ""))
+            #I'm so sorry
+            cleanfile.write(line.replace("How long is the imaging session (answer in seconds; recording will start after pressing ENTER)?", "").replace("How long is the imaging session (answer in seconds and press ENTER; time from TTL HIGH to end of the aqusition)?", "").replace(", array('B', [", "").replace("])", "").replace(",", ""))
